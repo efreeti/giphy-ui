686b1f7521c8b32810ee5d6f01ad86fc3f83db89
Bohdan Tsymbala <bohdan.tsymbala@ing.nl>
2019-11-16 15:58:26 +0100

    Initial app layout setup with material components.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <md-app md-mode="fixed">
+    <md-app-toolbar class="md-layout md-alignment-center-center">
+    </md-app-toolbar>
+    <md-app-content class="md-layout md-alignment-center-center">
+    </md-app-content>
+  </md-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.md-app {
+  max-height: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue';
-import App from './App.vue';
+// @ts-ignore
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
 import store from './store';
+import App from './App.vue';
+
 
 Vue.config.productionTip = false;
+Vue.use(VueMaterial);
 
 new Vue({
   store,
