ccaa4bfb64413762750da7dc5cebbf6ba4debbab
Bohdan Tsymbala <bohdan.tsymbala@ing.nl>
2019-11-17 10:21:45 +0100

    Added mapping of store to app.

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,31 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+
+import { SearchResultPageData } from '@/models/SearchResultPageData';
+
+const app = namespace('app');
 
 @Component({
   components: {
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  @app.State private searchText!: string;
+
+  @app.State private searchResultPages!: Array<SearchResultPageData>;
+
+  @app.State private searchResultPageSize!: number;
+
+  @app.Mutation private updateSearchText!: (searchText: string) => void;
+
+  @app.Action private updateSearchResults!: () => void;
+
+  @app.Action private showOneMorePage!: () => void;
+
+  @app.Getter private hasMoreSearchResultPages!: () => boolean;
+}
 </script>
 
 <style>
