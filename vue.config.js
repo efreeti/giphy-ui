cb9b57e51d5c07cf297ad5ed234972412c754606
Bohdan Tsymbala <bohdan.tsymbala@ing.nl>
2019-11-17 08:37:47 +0100

    Added sass processor.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require('path');
+const glob = require('glob');
+
 module.exports = {
   lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: glob.sync('node_modules/@material')
+            .map(d => path.join(__dirname, d)),
+        }
+      },
+    },
+  },
 };
